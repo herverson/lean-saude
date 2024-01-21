892d56cebcf76c1c2e847c2430ef1f77dabe514c
Herverson de Sousa <herverson.sousa@gmail.com>
2024-01-21 00:35:35 -0300

    login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Box,
+  IconButton,
+  InputAdornment,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+import styles from "./styles.module.scss";
+
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <Container className={styles.container}>
+      <Box className={styles.loginBox}>
+        <Typography variant="h4" color="primary" className={styles.logoTitle}>
+          LOGO
+        </Typography>
+        <Typography variant="h4" color="primary" className={styles.title}>
+          Bem-vindo(a)
+        </Typography>
+        <Typography
+          variant="h6"
+          color="primary"
+          className={styles.sectionTitle}
+        >
+          Acesse sua conta para iniciar a sessão
+        </Typography>
+        <TextField
+          label="Email"
+          placeholder="Email"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Senha"
+          placeholder="Senha"
+          type={showPassword ? "text" : "password"}
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleTogglePasswordVisibility}>
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Link href="#" passHref>
+          <Typography
+            variant="body2"
+            color="primary"
+            className={styles.forgotPassword}
+          >
+            Esqueceu a senha?
+          </Typography>
+        </Link>
+        <Button
+          variant="contained"
+          color="primary"
+          className={styles.loginButton}
+        >
+          Acessar plataforma
+        </Button>
+      </Box>
+      <Box className={styles.rightContainer}></Box>
+    </Container>
+  );
+}
